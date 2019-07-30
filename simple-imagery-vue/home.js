cb49e9230903674bbaff58adb0622681c4fd4153
yusoofash <41569614+yusoofash@users.noreply.github.com>
2019-07-30 06:17:31 +0530

    added imagery vue app

---
+++ simple-imagery-vue/home.js
@@ -0,0 +1,45 @@
+const HomeComponent = Vue.component('home', {
+    props: ['name'],
+    template: `
+    <div>
+        <div v-if="memeArr" class="card-columns">
+            <div class="d-inline-block m-1 image-block" v-for="meme of memeArr">
+                <img class="img-fluid" v-bind:src="meme.url" alt="Meme Image">
+                <p class="lead text-center text-white">{{meme.name}}</p>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            memeArr: null,
+            originMemeArr: null
+        }
+    },
+    methods: {
+        getMemes() {
+            return fetch('https://api.imgflip.com/get_memes')
+                .then(res => res.json());
+        }
+    },
+    created() {
+        this.getMemes()
+            .then(res => {
+                this.originMemeArr = this.memeArr = res.data.memes.slice(0, 12);
+            })
+    },
+    watch: {
+        name(cur) {
+            if (cur === undefined) this.memeArr = this.originMemeArr;
+
+            else {
+                this.memeArr = this.originMemeArr.filter(val => {
+                    console.log(val.name.toLowerCase(), cur.toLowerCase());
+                    return val.name.toLowerCase().includes(cur.toLowerCase())
+                });
+            }
+
+            console.log(cur);
+        }
+    }
+});
